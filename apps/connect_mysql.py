c9f3a224bfe5ee515d7515f57887ec4130a1a20b
leefneg <leefengme@163.com>
2018-03-27 10:04:14 +0800

    定时任务设置为查询

--- apps/connect_mysql.py
+++ apps/connect_mysql.py
@@ -4,5 +4,4 @@ from .core import db
 
 def connect():
     with app.app_context():
-        db.session.rollback()
-        print(datetime.datetime.now(),'定时任务，回滚链接一下数据库')
+        print(datetime.datetime.now(),'定时任务，链接一下数据库',db.session.execute('select * from alembic_version'))
