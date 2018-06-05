7a0db38f388dd27e6cc73b9d7fb111b8c0399453
leefneg <leefengme@163.com>
2018-06-05 16:01:43 +0800

    日志打印

--- apps/youheng/AppApi.py
+++ apps/youheng/AppApi.py
@@ -1,12 +1,15 @@
 
 from flask import jsonify,g,request
+import logging
 import json
 from apps.base.BaseRequest import BaseRequest
 
 
 class AppApi(BaseRequest):
+
     def post(self):
         phone = request.values.get('phone')
         msg = request.values.get('msg')
-        print(phone,msg)
+        logging.log(logging.WARNING,phone+'=='+ msg)
+
         pass
