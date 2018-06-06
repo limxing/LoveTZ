71a4f3dfabdedfb06343fbe0b2f90818a886acbc
leefneg <leefengme@163.com>
2018-06-06 12:52:07 +0800

    udid

--- apps/youheng/UdidApi.py
+++ apps/youheng/UdidApi.py
@@ -4,12 +4,14 @@ import logging
 from  apps.main.Result import Result
 import json
 from apps.base.BaseRequest import BaseRequest
+from xml.dom import minidom as dom
 
 
 class UdidApi(BaseRequest):
 
     def post(self):
-        print(str(request.data))
-        # print(bytes.decode(request.data,encoding=''))
+        print(type(request.data))
+        # print(bytes.decode(request.data))
 
+        # dom.parseString()
         return redirect('/static/udid.html',code=301)
