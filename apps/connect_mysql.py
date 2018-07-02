116a1508379ddfa1a1c2eb5f16b8a0784f26aa94
leefneg <leefengme@163.com>
2018-07-02 09:55:02 +0800

    发送早安报错

--- apps/connect_mysql.py
+++ apps/connect_mysql.py
@@ -2,6 +2,7 @@ import datetime
 from apps.facotry import app
 from .core import db
 import time
+import logging
 
 from apps.main.models import YouhengDuyao
 from apps.youheng.wechat import bot
@@ -15,8 +16,12 @@ def morning():
         duYao = db.session.query(YouhengDuyao).filter_by(isSend=False, type=0).first()
         quns = bot.groups().search('有恒')
         for qun in quns:
-            qun.send(duYao.text + '\n各位早安/:sun')
-            time.sleep(1)
+            try:
+                qun.send(duYao.text + '\n各位早安/:sun')
+                time.sleep(1)
+            except Exception as e:
+                logging.log(logging.ERROR, e)
+                pass
         duYao.isSend = True
         duYao.time_send = datetime.datetime.now()
         db.session.commit()
@@ -28,8 +33,12 @@ def night():
         duYao = db.session.query(YouhengDuyao).filter_by(isSend=False,type=1).first()
         quns = bot.groups().search('有恒')
         for qun in quns:
-            qun.send(duYao.text + '\n各位晚安 /:moon')
-            time.sleep(1)
+            try:
+                qun.send(duYao.text + '\n各位晚安 /:moon')
+                time.sleep(1)
+            except Exception as e:
+                logging.log(logging.ERROR, e)
+                pass
         duYao.isSend = True
         duYao.time_send = datetime.datetime.now()
         db.session.commit()
